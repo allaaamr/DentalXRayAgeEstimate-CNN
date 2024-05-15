33052856bad42d4a6270a56e189fab072d114f04
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2024-05-15 17:27:11 +0300

    Custom Data Set Fix

--- DentalXRayDataset.py
+++ DentalXRayDataset.py
@@ -6,12 +6,12 @@ import pandas as pd
 import os
 
 class DentalXRayDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None, shuffle=None, seed = 7):
+    def __init__(self, csv_file:str, root_dir:str, transform=None):
         """
 		Args:
-			csv_file (string): Path to the csv file with annotations.
+			csv_file (string): Path to the csv file with age labels.
             root_dir (string): Path to the root directory of images.
-            transform (callable, optional): A function/transform that takes in an PIL image
+            transform (callable): A function/transform that takes in an PIL image
                 and returns a transformed version. E.g, ``transforms.ToTensor()``.
 		"""
         self.data = pd.read_csv(csv_file)
@@ -23,8 +23,12 @@ class DentalXRayDataset(Dataset):
 
     def __getitem__(self, idx):
         img_name = os.path.join(self.root_dir, self.data.iloc[idx, 0])
-        image = Image.open(img_name).transform()
+        image = Image.open(img_name)
         age = self.data.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
         return image, torch.tensor(age, dtype=torch.float32)
+
+    # def load_image(self, index:int) -> Image.Image:
+    #     #Opens an image via a path and returns it
+    #    
